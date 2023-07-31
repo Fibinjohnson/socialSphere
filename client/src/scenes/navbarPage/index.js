08d680149147ce1018d44a5a4ef8ab36da935a92
Fibin Johnson <128117075+Fibinjohnson@users.noreply.github.com>
2023-07-31 22:19:40 +0530

    search feature

--- client/src/scenes/navbarPage/index.js
+++ client/src/scenes/navbarPage/index.js
@@ -58,7 +58,7 @@ const NavPage=()=>{
                 },
               }}
             >
-              SocioSphere
+               connect SocioSphere 
             </Typography>
             {isNonMobileScreen && (
               
